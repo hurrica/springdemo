6db281a838050f2b9cef18590eb1bce0c269fe83
robin <Robin.Chen@bluepay.asia>
2018-05-30 14:47:05 +0800

    test

---
+++ src/main/java/com/bluepay/spring/cache/RedisLockService.java
@@ -0,0 +1,37 @@
+package com.bluepay.spring.cache;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisLockService {
+    @Autowired
+    private JedisService jedisService;
+
+    private static final String LOCK_SUCCESS = "OK";
+    private static final String SET_IF_NOT_EXIST = "NX";
+    private static final String SET_WITH_EXPIRE_TIME = "EX";
+    private static final Long RELEASE_SUCCESS = 1L;
+
+    public String getRequestId(String id){
+        return id + "_" + System.currentTimeMillis();
+    }
+
+    public boolean tryGetRedisLock(String key, String value, int expiredTime){
+        jedisService.del(key);
+        String result = jedisService.set(key, value, SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, expiredTime);
+        if (LOCK_SUCCESS.equals(result)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean releaseRedisLock(String key, String value){
+        String script = "if redis.call('get', KEYS[1]) == KEYS[2] then return redis.call('del', KEYS[1]) else return 0 end";
+        Object result = jedisService.eval(script, 2, key, value);
+        if (RELEASE_SUCCESS.equals(result)){
+            return true;
+        }
+        return false;
+    }
+}
--- src/main/java/com/bluepay/spring/demo/TestMain.java
+++ src/main/java/com/bluepay/spring/demo/TestMain.java
@@ -1,9 +1,33 @@
 package com.bluepay.spring.demo;
 
 
+import org.apache.commons.codec.binary.BinaryCodec;
+
+import java.io.UnsupportedEncodingException;
 
 public class TestMain {
-    public static void main(String[] args) {
-        System.out.println(System.currentTimeMillis());
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String ss = "测试";
+        byte[] utf8 = ss.getBytes("utf-8");
+        System.out.println(arrayToString(utf8) + "    " + new String(utf8));
+        byte[] ascii = ss.getBytes("ASCII");
+        System.out.println(arrayToString(ascii) + "   " + new String(ascii));
+        byte[] iso88591 = ss.getBytes("iso-8859-1");
+        System.out.println(arrayToString(iso88591) + "    " + new String(iso88591));
+    }
+
+    public static String arrayToString(byte[] array){
+        StringBuilder stringBuilder = new StringBuilder(array.length);
+        for (int i = 0; i < array.length; i++) {
+            stringBuilder.append(array[i] + " ");
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void test() throws UnsupportedEncodingException {
+        String ss = "测试";
+        String ascii = BinaryCodec.toAsciiString(ss.getBytes());
+        System.out.println(ascii);
+        System.out.println(new String(BinaryCodec.fromAscii(ascii.getBytes()), "utf-8"));
     }
 }
