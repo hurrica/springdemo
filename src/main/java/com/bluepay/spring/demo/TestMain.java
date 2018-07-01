1d976d107e8d0be1122094848da6a31382bd8447
ping.chen <1411976412@qq.com>
2018-07-01 10:28:59 +0800

    'a'

--- src/main/java/com/bluepay/spring/demo/TestMain.java
+++ src/main/java/com/bluepay/spring/demo/TestMain.java
@@ -1,9 +1,14 @@
 package com.bluepay.spring.demo;
 
 
+import org.apache.zookeeper.ZooKeeper;
 
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("111".equals(null));
+        byte[] bytes = new byte[1024*1024*20];
+        System.gc();
+        System.out.println(Runtime.getRuntime().maxMemory()/1024/1024);
+        System.out.println(Runtime.getRuntime().freeMemory()/1024/1024);
+        System.out.println(Runtime.getRuntime().totalMemory()/1024/1024);
     }
 }
